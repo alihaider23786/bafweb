0d1366c0ec713b03f0e25dfe82595ba5dbe571a6
alihaider23786 <141299051+alihaider23786@users.noreply.github.com>
2023-09-14 02:49:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/backend',backend );
 
 
 // Start the server
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || ;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
